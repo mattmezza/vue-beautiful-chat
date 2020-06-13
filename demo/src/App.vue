fc3306dc37dcbbdd512f47a78105d89e398476ef
Matteo Merola <mattmezza@gmail.com>
2020-06-13 12:06:22 +0200

    Added template for system messages.

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -40,6 +40,9 @@
           <template v-if="scopedProps.message.liked">👍</template>
         </p>
       </template>
+      <template v-slot:system-message-body="{ message }">
+        [System]: {{message.text}}
+      </template>
     </beautiful-chat>
     <p class="text-center toggle">
       <a
