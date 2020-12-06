ead7206e0e83249927c7fe5fea3368433da2a8ff
Pavlo Maks <pavlo@solidstategroup.com>
2020-12-06 23:54:06 +0000

    styles changes

--- src/ChatWindow.vue
+++ src/ChatWindow.vue
@@ -168,7 +168,7 @@ export default {
   bottom: 100px;
   box-sizing: border-box;
   box-shadow: 0px 7px 40px 2px rgba(148, 149, 150, 0.1);
-  background: white;
+  /*background: white;*/
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -177,6 +177,8 @@ export default {
   animation: fadeIn;
   animation-duration: 0.3s;
   animation-timing-function: ease-in-out;
+  background-color: #f9fafa;
+  border: 1px solid #dcdcdc;
 }
 
 .sc-chat-window.closed {
